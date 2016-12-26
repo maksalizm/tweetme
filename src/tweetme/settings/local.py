3685f2661a62e4f0eb0731f6e3ec1a71edb7f920
maksalizm <maksalizm@gmail.com>
2016-12-26 22:11:01 +0900

    tweet 앱 생성

--- src/tweetme/settings/local.py
+++ src/tweetme/settings/local.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# manage.py location
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tweets',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'tweetme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    #will not be served, long term storage
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+# will be serve
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-server")
+
+# STATIC_ROOT = "/webapps/abc/static"
---
+++ src/tweets/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Tweet(models.Model):
+    content = models.TextField()
+    content2 = models.TextField()
+
