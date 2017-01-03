820259998407081be09e7a02eb0d23d29ebb8bc7
maksalizm <maksalizm@gmail.com>
2017-01-04 00:45:28 +0900

    crispy library 적용

--- src/tweetme/settings/local.py
+++ src/tweetme/settings/local.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 
     'tweets',
 ]
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-server")
 
 # STATIC_ROOT = "/webapps/abc/static"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- src/tweetme/settings/production.py
+++ src/tweetme/settings/production.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+
+    'tweets',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
