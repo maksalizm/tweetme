5d31ef0724db1c30b8fd56d4249ff58d835ebf70
maksalizm <maksalizm@gmail.com>
2017-01-07 17:21:30 +0900

    crate restApiList

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from .views import (
+    TweetListAPIView
     # TweetDetailView,
     # TweetListView,
     # TweetCreateView,
@@ -10,7 +11,7 @@ from .views import (
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
-    # url(r'^search/$', TweetListView.as_view(), name="list"),
+    url(r'^$', TweetListAPIView.as_view(), name="list"), #/api/tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name="create"),
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name="detail"),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name="update"),
--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -9,7 +9,7 @@ from .views import (
 )
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url="/")),
+    # url(r'^$', RedirectView.as_view(url="/")),
     url(r'^search/$', TweetListView.as_view(), name="list"),
     url(r'^create/$', TweetCreateView.as_view(), name="create"),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name="detail"),
