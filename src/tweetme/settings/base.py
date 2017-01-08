169baff181a7bc0714f7955a2d292d06de7bd1d7
maksalizm <maksalizm@gmail.com>
2017-01-08 16:08:54 +0900

    creatview API 작업 완료

--- src/tweetme/settings/base.py
+++ src/tweetme/settings/base.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '@v_g$@c%ny#&uenyr0!ikb7ajod0_y#^a4cq5+tnns7nm#jg9)'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'rest_framework',
-
 
     'accounts',
     'tweets',
@@ -76,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tweetme.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -86,7 +82,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -106,20 +101,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
+# USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,20 +1,30 @@
 from rest_framework import generics
+from rest_framework import permissions
+
 from django.db.models import Q
 
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
 
+class TweetCreateAPIView(generics.CreateAPIView):
+    serializer_class = TweetModelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
     def get_queryset(self):
-        qs = Tweet.objects.all()
+        qs = Tweet.objects.all().order_by('-timestamp')
         query = self.request.GET.get("q", None)
         print query
         if query is not None:
             qs = qs.filter(
-                Q(content__icontains=query)|
+                Q(content__icontains=query) |
                 Q(user__username__icontains=query)
             )
         return qs
