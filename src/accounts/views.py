3ddedd04b961f77582634ab1a608d044a8e09f86
maksalizm <maksalizm@gmail.com>
2017-01-10 00:02:16 +0900

    Toggle Follow

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+from django.views import View
 from django.views.generic import DetailView
 
 # Create your views here.
+
+from .models import UserProfile
 
 User = get_user_model()
 
@@ -13,3 +17,19 @@ class UserDetailView(DetailView):
 
     def get_object(self):
         return get_object_or_404(User, username__iexact=self.kwargs.get('username'))
+
+
+class UserFollowView(View):
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        print toggle_user
+        if request.user.is_authenticated():
+            user_profile, created = UserProfile.objects.get_or_create(user=request.user)  # (user_obj, true)
+            if toggle_user in user_profile.following.all():
+                user_profile.following.remove(toggle_user)
+            else:
+                user_profile.following.add(toggle_user)
+        return redirect("profile:detail", username=username)
+
+        # url = reverse("profile:detail", kwargs={"username":useranme}
+        # HttpResponseRedirect(url)
